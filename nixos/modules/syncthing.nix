66ade948ca84c77b72590166075a87b96270fed4
p-laranjinha <plcasimiro2000@gmail.com>
2024-12-04 23:00:10 +0000

    113 2024-12-04 23:00:05

--- nixos/modules/syncthing.nix
+++ nixos/modules/syncthing.nix
@@ -2,9 +2,11 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
+    user = "pebble";
+    dataDir = "/home/pebble";
     settings = {
       devices = {
-        "phone".id = "JU2TRHN-NJQPZUH-QPBTFZB-IEEDJKW-45V5IRB-6YSUSUS-OVWWEK3-Y4QPBAS";
+        phone.id = "JU2TRHN-NJQPZUH-QPBTFZB-IEEDJKW-45V5IRB-6YSUSUS-OVWWEK3-Y4QPBAS";
       };
       folders = {
         "Tachiyomi Backup" = {
@@ -23,6 +25,7 @@
     syncthingtray
   ];
   systemd.user.services.syncthingtray = {
+    enable = false;
     description = "Launch SyncthingTray on startup.";
     script = ''${pkgs.syncthingtray}/bin/syncthingtray --wait'';
     wantedBy = ["graphical-session.target"];
