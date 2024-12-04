aa808388bcbcb673543df0ab1bbbdc135e2e23e2
p-laranjinha <plcasimiro2000@gmail.com>
2024-12-04 23:24:16 +0000

    118 2024-12-04 23:24:11

--- nixos/modules/syncthing.nix
+++ nixos/modules/syncthing.nix
@@ -4,16 +4,46 @@
     openDefaultPorts = true;
     user = "pebble";
     dataDir = "/home/pebble";
-    overrideDevices = true;
-    overrideFolders = false;
     settings = {
       devices = {
-        "phone" = {id = "JU2TRHN-NJQPZUH-QPBTFZB-IEEDJKW-45V5IRV-6YSUSUS-OVWWEK3-Y4QPBAS";};
+        "phone".id = "JU2TRHN-NJQPZUH-QPBTFZB-IEEDJKW-45V5IRV-6YSUSUS-OVWWEK3-Y4QPBAS";
+        "tablet".id = "XR6AZSI-APGKKIB-LWMMCKW-6E63TBX-KFBSLVG-7JYURKQ-TZNGESZ-IVNNDQ5";
       };
       folders = {
+        "Default" = {
+          id = "default";
+          path = "~/Sync/Default";
+          devices = [
+            "phone"
+            "tablet"
+          ];
+        };
+        "Obsidian Vaults" = {
+          id = "obsidian-vaults";
+          path = "~/Obsidian Vaults";
+          devices = [
+            "phone"
+            "tablet"
+          ];
+        };
+        "Music" = {
+          id = "music";
+          path = "~/Music/Music";
+          devices = [
+            "phone"
+          ];
+        };
         "Tachiyomi Backup" = {
           id = "tachiyomi-backup";
           path = "~/Sync/Tachiyomi Backup";
+          devices = [
+            "phone"
+            "tablet"
+          ];
+        };
+        "WIT" = {
+          id = "wit";
+          path = "~/Sync/WIT";
           devices = [
             "phone"
           ];
