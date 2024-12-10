2c8e75c04047340cb33f1df514027a15db36ed76
p-laranjinha <plcasimiro2000@gmail.com>
2024-12-10 17:42:52 +0000

    177 2024-12-10 17:42:46

--- home-manager/modules/packages.nix
+++ home-manager/modules/packages.nix
@@ -9,20 +9,82 @@
 
   programs.firefox = {
     enable = true;
+    # Check about:policies#documentation for options.
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisablePocket = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DontCheckDefaultBrowser = true;
+      DisplayBookmarksToolbar = "never"; # "never" "always" "newtab"
+      DisplayMenuBar = "default-off"; # "default-off" "always" "never" "default-on"
+      SearchBar = "unified"; # "unified" "separate"
+      NoDefaultBookmarks = true;
+      OfferToSaveLoginsDefault = false;
+      PasswordManagerEnabled = false;
+    };
     profiles.default = {
+      #preferences = {
+      #  "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+      #  "extensions.pocket.enabled" = false;
+      #  "extensions.screenshots.disabled" = true;
+      #  "browser.topsites.contile.enabled" = false;
+      #  "browser.formfill.enable" = false;
+      #  "browser.search.suggest.enabled" = false;
+      #  "browser.search.suggest.enabled.private" = false;
+      #  "browser.urlbar.suggest.searches" = false;
+      #  "browser.urlbar.showSearchSuggestionsFirst" = false;
+      #};
       search = {
         force = true;
-        #engines = {};
+        default = "DuckDuckGo";
         order = [
           "DuckDuckGo"
           "Google"
         ];
+        engines = {
+          "Nix Packages" = {
+            urls = [
+              {
+                template = "https://search.nixos.org/packages";
+                params = [
+                  {
+                    name = "type";
+                    value = "packages";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+            icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = ["@np"];
+          };
+          "NixOS Wiki" = {
+            urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
+            iconUpdateURL = "https://nixos.wiki/favicon.png";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = ["@nw"];
+          };
+          "Bing".metaData.hidden = true;
+          "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias.
+        };
       };
       settings = {};
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
         bitwarden
         ublock-origin
         darkreader
+        # The rest can just be installed by logging in to the Firefox account.
       ];
     };
   };
