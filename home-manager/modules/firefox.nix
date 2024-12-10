7a26788bf76bf13747adaebead1a2f52f255b03b
p-laranjinha <plcasimiro2000@gmail.com>
2024-12-10 20:17:01 +0000

    181 2024-12-10 20:16:56

---
+++ home-manager/modules/firefox.nix
@@ -0,0 +1,136 @@
+{pkgs, ...}: {
+  programs.firefox = {
+    enable = true;
+    # Check about:policies#documentation for options.
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisablePocket = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DontCheckDefaultBrowser = true;
+      DisplayBookmarksToolbar = "never"; # "never" "always" "newtab"
+      DisplayMenuBar = "default-off"; # "default-off" "always" "never" "default-on"
+      SearchBar = "unified"; # "unified" "separate"
+      NoDefaultBookmarks = true;
+      OfferToSaveLoginsDefault = false;
+      PasswordManagerEnabled = false;
+    };
+    profiles.default = {
+      settings = {
+        "browser.tabs.closeWindowWithLastTab" = false;
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.globalprivacycontrol.enabled" = true;
+        "extensions.formautofill.addresses.enabled" = false;
+        "extensions.formautofill.creditCards.enabled" = false;
+        "dom.security.https_only_mode" = true;
+        "dom.security.https_only_mode_ever_enabled" = true;
+        "browser.urlbar.maxRichResults" = 20;
+        "browser.urlbar.showSearchSuggestionsFirst" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.uiCustomization.state" = builtins.toJSON {
+          placements = {
+            widget-overflow-fixed-list = [
+            ];
+            unified-extensions-area = [
+              "ublock0_raymondhill_net-browser-action"
+              "_aecec67f-0d10-4fa7-b7c7-609a2db280cf_-browser-action"
+              "plasma-browser-integration_kde_org-browser-action"
+            ];
+            nav-bar = [
+              "back-button"
+              "forward-button"
+              "stop-reload-button"
+              "customizableui-special-spring1"
+              "urlbar-container"
+              "customizableui-special-spring2"
+              "save-to-pocket-button"
+              "downloads-button"
+              "fxa-toolbar-menu-button"
+              "unified-extensions-button"
+              "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"
+              "addon_darkreader_org-browser-action"
+            ];
+            toolbar-menubar = [
+              "menubar-items"
+            ];
+            TabsToolbar = [
+              "firefox-view-button"
+              "tabbrowser-tabs"
+              "new-tab-button"
+              "alltabs-button"
+            ];
+            vertical-tabs = [
+            ];
+            PersonalToolbar = [
+              "import-button"
+              "personal-bookmarks"
+            ];
+          };
+          seen = [];
+          dirtyAreaCache = [
+            "nav-bar"
+            "vertical-tabs"
+            "PersonalToolbar"
+            "unified-extensions-area"
+            "toolbar-menubar"
+            "TabsToolbar"
+          ];
+          "currentVersion" = 1;
+          "newElementCount" = 0;
+        };
+        #"browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["ublock0_raymondhill_net-browser-action","_aecec67f-0d10-4fa7-b7c7-609a2db280cf_-browser-action","plasma-browser-integration_kde_org-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","customizableui-special-spring1","urlbar-container","customizableui-special-spring2","save-to-pocket-button","downloads-button","fxa-toolbar-menu-button","unified-extensions-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","addon_darkreader_org-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","new-tab-button","alltabs-button"],"vertical-tabs":[],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","addon_darkreader_org-browser-action","ublock0_raymondhill_net-browser-action","_aecec67f-0d10-4fa7-b7c7-609a2db280cf_-browser-action","plasma-browser-integration_kde_org-browser-action"],"dirtyAreaCache":["nav-bar","vertical-tabs","PersonalToolbar","unified-extensions-area","toolbar-menubar","TabsToolbar"],"currentVersion":20,"newElementCount":2}'';
+      };
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        bitwarden
+        ublock-origin
+        darkreader
+        # The rest can just be installed by logging in to the Firefox account.
+      ];
+      search = {
+        force = true;
+        default = "DuckDuckGo";
+        order = [
+          "DuckDuckGo"
+          "Google"
+        ];
+        engines = {
+          "Nix Packages" = {
+            urls = [
+              {
+                template = "https://search.nixos.org/packages";
+                params = [
+                  {
+                    name = "type";
+                    value = "packages";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+            icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = ["@np"];
+          };
+          "NixOS Wiki" = {
+            urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
+            iconUpdateURL = "https://nixos.wiki/favicon.png";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = ["@nw"];
+          };
+          "Bing".metaData.hidden = true;
+          "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias.
+        };
+      };
+    };
+  };
+}
--- home-manager/modules/packages.nix
+++ home-manager/modules/packages.nix
@@ -3,92 +3,13 @@
   inputs,
   ...
 }: {
+  imports = [
+    ./firefox.nix
+  ];
+
   programs.bash.enable = true;
 
   programs.zoxide.enable = true;
-
-  programs.firefox = {
-    enable = true;
-    # Check about:policies#documentation for options.
-    policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      EnableTrackingProtection = {
-        Value = true;
-        Locked = true;
-        Cryptomining = true;
-        Fingerprinting = true;
-      };
-      DisablePocket = true;
-      OverrideFirstRunPage = "";
-      OverridePostUpdatePage = "";
-      DontCheckDefaultBrowser = true;
-      DisplayBookmarksToolbar = "never"; # "never" "always" "newtab"
-      DisplayMenuBar = "default-off"; # "default-off" "always" "never" "default-on"
-      SearchBar = "unified"; # "unified" "separate"
-      NoDefaultBookmarks = true;
-      OfferToSaveLoginsDefault = false;
-      PasswordManagerEnabled = false;
-    };
-    profiles.default = {
-      settings = {
-        "browser.tabs.closeWindowWithLastTab" = false;
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.globalprivacycontrol.enabled" = true;
-        "extensions.formautofill.addresses.enabled" = false;
-        "extensions.formautofill.creditCards.enabled" = false;
-        "dom.security.https_only_mode" = true;
-        "dom.security.https_only_mode_ever_enabled" = true;
-        "browser.urlbar.maxRichResults" = 20;
-        "browser.urlbar.showSearchSuggestionsFirst" = false;
-        "browser.newtabpage.activity-stream.showSponsored" = false;
-        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-      };
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-        bitwarden
-        ublock-origin
-        darkreader
-        # The rest can just be installed by logging in to the Firefox account.
-      ];
-      search = {
-        force = true;
-        default = "DuckDuckGo";
-        order = [
-          "DuckDuckGo"
-          "Google"
-        ];
-        engines = {
-          "Nix Packages" = {
-            urls = [
-              {
-                template = "https://search.nixos.org/packages";
-                params = [
-                  {
-                    name = "type";
-                    value = "packages";
-                  }
-                  {
-                    name = "query";
-                    value = "{searchTerms}";
-                  }
-                ];
-              }
-            ];
-            icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = ["@np"];
-          };
-          "NixOS Wiki" = {
-            urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
-            iconUpdateURL = "https://nixos.wiki/favicon.png";
-            updateInterval = 24 * 60 * 60 * 1000; # every day
-            definedAliases = ["@nw"];
-          };
-          "Bing".metaData.hidden = true;
-          "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias.
-        };
-      };
-    };
-  };
 
   # The home.packages option allows you to install Nix packages into your environment.
   home.packages = with pkgs; [
