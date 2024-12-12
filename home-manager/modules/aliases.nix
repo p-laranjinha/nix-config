ec4d054d921acfa9249ea79a30a2dce806d01fa4
p-laranjinha <plcasimiro2000@gmail.com>
2024-12-12 22:09:10 +0000

    197 2024-12-12 22:09:04

--- home-manager/modules/aliases.nix
+++ home-manager/modules/aliases.nix
@@ -1,6 +1,5 @@
 {...}: {
   home.shellAliases = {
     rebuild = "~/nixos/rebuild.sh";
-    #cq-editor = "nix run github:marcus7070/cq-flake";
   };
 }
--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -3,6 +3,9 @@
     url = "https://github.com/p-laranjinha/neovim-config";
     sha256 = "sha256-O2HeUy1BHcufYcUmeI9jO0KT9xuFGRXW7ILZ7sel7C4=";
   };
+  home.shellAliases = {
+    nvimu = "nvim -u ~/Programs/neovim-config/init.lua";
+  };
   programs.neovim = {
     enable = true;
     defaultEditor = true;
