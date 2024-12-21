976eeca18eecc015365770b4fb6dbfaa6fb4f487
p-laranjinha <plcasimiro2000@gmail.com>
2024-12-21 18:14:17 +0000

    236 2024-12-21 18:14:09

--- home-manager/modules/betterbird.nix
+++ home-manager/modules/betterbird.nix
@@ -1,8 +1,8 @@
 {lib, ...}: let
   # This commented line gets the profile automatically but is inpure.
-  #profile = lib.lists.findFirst (value: (lib.strings.hasSuffix "default-default" value)) "" (lib.mapAttrsToList (name: value: name) (builtins.readDir /home/pebble/.var/app/eu.betterbird.Betterbird/.thunderbird));\
+  #profile = lib.lists.findFirst (value: (lib.strings.hasSuffix "default-default" value)) "" (lib.mapAttrsToList (name: value: name) (builtins.readDir /home/pebble/.var/app/eu.betterbird.Betterbird/cache/thunderbird));\
   profile = "1h50fcaa.default-default";
-  userjs = lib.strings.concatStrings [".var/app/eu.betterbird.Betterbird/.thunderbird/" profile "/user.js"];
+  userjs = lib.strings.concatStrings [".var/app/eu.betterbird.Betterbird/cache/thunderbird/" profile "/user.js"];
 in {
   services.flatpak.packages = [
     "eu.betterbird.Betterbird"
@@ -10,6 +10,6 @@ in {
 
   home.file."${userjs}".text = ''
     user_pref("mail.startupMinimized", true);
-    user_pref("mail.minimizeToTray", false);
+    user_pref("mail.minimizeToTray", true);
   '';
 }
