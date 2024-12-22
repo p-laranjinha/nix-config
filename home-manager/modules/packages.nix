438d81b073e5c1e7faf0dea57c398dd4d4cce3a0
p-laranjinha <plcasimiro2000@gmail.com>
2024-12-22 03:31:38 +0000

    264 2024-12-22 03:31:30

--- home-manager/modules/packages.nix
+++ home-manager/modules/packages.nix
@@ -71,8 +71,16 @@
       ]))
     # Autostart scripts.
     (builtins.listToAttrs (builtins.map (x: {
-        name = ".config/autostart/${x.name}.sh";
-        value = {source = pkgs.writeShellScript x.name x.script;};
+        name = ".config/autostart/${x.name}.desktop";
+        value = {
+          text = ''
+            [[Desktop Entry]]
+            Exec=${pkgs.writeShellScript x.name x.script}
+            Name=${x.name}
+            Type=Application
+            X-KDE-AutostartScript=true
+          '';
+        };
       })
       [
         {
