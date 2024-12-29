29e8735f8b21877612ce76caa7c12cc510097d02
p-laranjinha <plcasimiro2000@gmail.com>
2024-12-29 01:19:58 +0000

    277 2024-12-29 01:19:50

--- home-manager/modules/ghostty.nix
+++ home-manager/modules/ghostty.nix
@@ -5,6 +5,9 @@
 
   home.file.".config/ghostty/config".text = ''
     theme = ayu
+    # The background color in nvim is darker, so I chose that.
+    background = #0A0E14
+
     font-size = 11
     font-family = "FiraCode Nerd Font"
   '';
