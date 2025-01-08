bdf13c5ccea5f3cac2ef3ffff20ad8c57fb11bca
p-laranjinha <plcasimiro2000@gmail.com>
2025-01-08 18:54:19 +0000

    298 2025-01-08 18:54:10

--- system/remote-access.nix
+++ system/remote-access.nix
@@ -12,10 +12,10 @@ in {
 
   environment.systemPackages = [
     # pkgs.kdePackages.krdp
-    pkgs.kdePackages.krfb
+    # pkgs.kdePackages.krfb
   ];
   # networking.firewall.allowedTCPPorts = [22 3389];
-  networking.firewall.allowedTCPPorts = [22 5900];
+  # networking.firewall.allowedTCPPorts = [22 5900];
 
   # networking.firewall.allowedTCPPorts = [22];
   services.openssh = {
