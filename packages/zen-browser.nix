cd753add78ce323637f1b0e053aeb119a0ec029a
p-laranjinha <plcasimiro2000@gmail.com>
2025-03-23 21:04:48 +0000

    326 2025-03-23 21:04:38

--- packages/zen-browser.nix
+++ packages/zen-browser.nix
@@ -61,8 +61,7 @@ in {
         "${config.home.homeDirectory}/.zen/default/zen-themes.json".text = "{${builtins.toString (
           builtins.map (theme: ''
             "${theme}":{
-              ${builtins.readFile "${zen-themes-repo}/themes/${theme}/theme.json"},
-              "enabled": true}${
+              ${builtins.toJSON ((builtins.fromJSON (builtins.readFile "${zen-themes-repo}/themes/${theme}/theme.json")) // {enabled = true;})}${
               if theme == builtins.tail zen-themes
               then ""
               else ","
