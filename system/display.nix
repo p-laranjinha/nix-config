7244b4b2f4b47a8abc21a23fb7d6a30b17dcf3bd
p-laranjinha <plcasimiro2000@gmail.com>
2025-03-30 21:42:25 +0100

    362 2025-03-30 21:42:14

--- system/display.nix
+++ system/display.nix
@@ -6,4 +6,6 @@
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  services.autorandr.enable = true;
 }
