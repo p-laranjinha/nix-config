32e4eb6e21703b7a600e609fe4bb5beaba036d40
p-laranjinha <plcasimiro2000@gmail.com>
2025-05-28 22:14:18 +0100

    406 2025-05-28 22:14:12

--- packages/terminal.nix
+++ packages/terminal.nix
@@ -7,7 +7,8 @@
   home.file.".config/ghostty/config".text = ''
     theme = ayu
     # The background color in nvim is darker, so I chose that.
-    background = #0A0E14
+    # background = #0A0E14
+    background = #000000
 
     font-size = 11
     font-family = "FiraCode Nerd Font"
