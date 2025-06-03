3b9d4e74ff87d8926ea8fb2c299ee2a3e441aa49
p-laranjinha <plcasimiro2000@gmail.com>
2025-06-03 15:58:03 +0100

    410 2025-06-03 15:57:55

--- packages/default-home.nix
+++ packages/default-home.nix
@@ -106,6 +106,9 @@
     # Game.
     mindustry
 
+    libsForQt5.partitionmanager
+    rpi-imager
+
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
