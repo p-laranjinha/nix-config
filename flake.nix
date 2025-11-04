3d1c4ed8686346b70cd7e7ec560690bb0a40c3da
p-laranjinha <plcasimiro2000@gmail.com>
2025-11-04 12:26:19 +0000

    492 2025-11-04 12:26:10

--- flake.nix
+++ flake.nix
@@ -2,13 +2,14 @@
   description = "flake";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     # plasma-manager currently requires nixos unstable :(
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      # url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
