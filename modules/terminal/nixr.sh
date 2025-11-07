38d589373ea68948d0f587af731cf3999053933b
p-laranjinha <plcasimiro2000@gmail.com>
2025-11-07 23:06:40 +0000

    Finished nixr script.

--- modules/terminal/nixr.sh
+++ modules/terminal/nixr.sh
@@ -17,8 +17,8 @@ fi
 git diff HEAD | delta --paging always
 gum log --time timeonly --level info "Finished showing diff."
 
-git commit -m "$(gum input --width 50 --header "Write commit summary:" --placeholder "")" \
-           -m "$(gum write --width 80 --header "Write commit description:" --placeholder "")"
+git commit -m "$(gum input --width 50 --header "Input commit summary:" --placeholder "")" \
+           -m "$(gum write --width 80 --header "Input commit description:" --placeholder "")"
 
 if [ $? -eq 0 ]; then
 	gum log --time timeonly --level info "Committed changes."
@@ -27,10 +27,20 @@ else
 	exit 1
 fi
 
-gum input --password | sudo -nS gum spin --title "Rebuilding." -- nixos-rebuild switch --flak $NIX_CONFIG_DIR
+gum log --time timeonly --level info "Rebuilding."
+sudo nixos-rebuild switch --flake $NIX_CONFIG_DIR
 if [ $? -eq 0 ]; then
 	gum log --time timeonly --level info "Finished rebuilding."
 else
 	gum log --time timeonly --level error "Failed rebuild."
+	exit 1
+fi
+
+gum log --time timeonly --level info "Pushing to origin."
+git push
+if [ $? -eq 0 ]; then
+	gum log --time timeonly --level info "Finished pushing."
+else
+	gum log --time timeonly --level error "Failed to push."
 	exit 1
 fi
