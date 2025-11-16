b7f22fde9daadde8e77b196c100f6bb2f216a95f
p-laranjinha <plcasimiro2000@gmail.com>
2025-11-16 21:41:57 +0000

    Started figuring out how podman namespaces work.
    Making this commit do my comments describing my previous thoughts get
    saved.

--- modules/containers/default.nix
+++ modules/containers/default.nix
@@ -2,6 +2,7 @@
   inputs,
   this,
   lib,
+  config,
   ...
 }: {
   imports =
@@ -14,6 +15,17 @@
     linger = true;
     # Required for rootless container with multiple users.
     autoSubUidGidRange = true;
+    subGidRanges = [
+      {
+        count = 1;
+        startGid = 1001;
+      }
+    ];
+  };
+  # A 'share' group for container users so they don't have to be mapped to the host user.
+  users.groups.share = {
+    gid = 1001;
+    members = [this.username];
   };
   hm = {
     imports = [inputs.quadlet-nix.homeManagerModules.quadlet];
@@ -22,3 +34,37 @@
     };
   };
 }
+# Container options:
+#  https://seiarotg.github.io/quadlet-nix/home-manager-options.html
+#
+# Volumes don't support symlinks, so when I want to volume folders in this repo
+#  it has to be directly.
+#
+# https://www.redhat.com/en/blog/rootless-podman-user-namespace-modes
+# I'm using 'userns="nomap"' for the most security, and if I stopped here I
+#  could make the volumes use the 'U' option so that the containers
+#  automatically change owner/group and can 'read from'/'write to' the volumes.
+# But the 'U' option makes it harder to edit volume files outside the
+#  containers because of the changed owner/group (requires sudo).
+#
+# https://www.redhat.com/en/blog/user-flag-rootless-containers
+# I'm also using 'user="<user>"' to make the container not use the root user,
+#  as even in rootless mode it still has more permissions. I find a valid user
+#  by running 'podman exec -it <container> cat /etc/passwd' to list all users
+#  (the best one to use is probably the last one).
+# For containers that need to be root to run some utils, I'll remove the user
+#  option but drop root's capabilities instead. Use the following to get the
+#  capabilities of the current user: 'podman top <container> capeff'
+# Changing the user or removing root capabilities makes the 'U' volume option
+#  stop working, which then requires sharing host groups with write permissions.
+#
+# https://www.redhat.com/en/blog/supplemental-groups-podman-containers
+# When I want to add the host user's groups to the container's user, I'll use
+#  'annotations = {"run.oci.keep_original_groups" = "1";};' instead of
+#  'addGroups = ["keep-groups"]; so that I can define other groups using
+#  'addGroups' if needed.
+#
+# I'm using 'systemd.tmpfiles.rules' to automatically create directories and
+#  set their permissions. Using 2___ permissions, makes it so the files created
+#  in that directory inherit the group, so I can hopefully at least read the
+#  files outside the container.
--- modules/containers/searxng/default.nix
+++ modules/containers/searxng/default.nix
@@ -3,15 +3,14 @@
   config,
   ...
 }: let
-  # Volumes don't support symlinks so this one will be inside this repo.
   searxng-config = config.lib.meta.relativeToAbsoluteConfigPath ./config;
   searxng-data = "${this.homeDirectory}/Desktop/searxng/data";
   valkey-data = "${this.homeDirectory}/Desktop/searxng/valkey-data";
 in {
-  # Make sure the volume directories are created.
   systemd.tmpfiles.rules = [
-    "d ${searxng-data} 0740 ${this.username} users - -"
-    "d ${valkey-data} 0740 ${this.username} users - -"
+    "d ${searxng-config} 2770 ${this.username} share - -"
+    "d ${searxng-data} 2770 ${this.username} share - -"
+    "d ${valkey-data} 2770 ${this.username} users - -"
   ];
   secrets.searxng = {
     sopsFile = ./secrets.env;
@@ -26,23 +25,6 @@ in {
   hm = {
     # https://seiarotg.github.io/quadlet-nix/home-manager-options.html
     virtualisation.quadlet = {
-      # https://www.redhat.com/en/blog/rootless-podman-user-namespace-modes
-      # I'm using 'userns="nomap"' for the most security, but that requires
-      #  that volumes use the 'U' option so that the containers automatically
-      #  change owner/group and can 'read from'/'write to' the volumes. But the
-      #  'U' option makes it harder to edit volume files outside the containers
-      #  because of the changed owner/group (requires sudo), and so I add the
-      #  'copy' option to volumes where I intend to manually edit the contents,
-      #  so that it copies the files on volume creation. The 'copy' option has
-      #  some side-effects: changes made to the volumes by the container are
-      #  forgotten on shutdown, and changes made outside the container only
-      #  affect the container after it restarts.
-      # https://www.redhat.com/en/blog/user-flag-rootless-containers
-      # I'm also using 'user="<user>"' to make the container not use the root
-      #  user, as even in rootless mode it still has more permissions. I find a
-      #  valid user by running 'podman exec -it <container> /bin/sh', and then
-      #  running 'cat /etc/passwd' to list all users (the best one to use is
-      #  probably the last one).
       containers = {
         searxng = {
           autoStart = true;
@@ -53,14 +35,38 @@ in {
           containerConfig = {
             image = "docker.io/searxng/searxng:latest";
             publishPorts = ["8080:8080"];
+            environments = {
+              FORCE_OWNERSHIP = "false";
+            };
             environmentFiles = [config.secrets.searxng.path];
             volumes = [
-              "${searxng-config}:/etc/searxng:U,copy"
-              "${searxng-data}:/var/cache/searxng:U"
+              "${searxng-config}:/etc/searxng"
+              "${searxng-data}:/var/cache/searxng"
             ];
             networks = ["searxng"];
-            userns = "nomap";
-            user = "searxng";
+            # userns = "nomap";
+            dropCapabilities = [
+              "CHOWN"
+              "DAC_OVERRIDE"
+              "FOWNER"
+              "FSETID"
+              "KILL"
+              "NET_BIND_SERVICE"
+              "SETFCAP"
+              "SETGID"
+              "SETPCAP"
+              "SETUID"
+              "SYS_CHROOT"
+            ];
+            # annotations = {"run.oci.keep_original_groups" = "1";};
+            uidMaps = [
+              "0:1:1000"
+            ];
+            gidMaps = [
+              "0:2:1000"
+              "1001:1:1"
+            ];
+            addGroups = ["1001"];
           };
         };
         searxng-valkey = {
@@ -72,11 +78,12 @@ in {
           containerConfig = {
             image = "docker.io/valkey/valkey:latest";
             exec = "valkey-server --save 30 1 --loglevel warning";
-            volumes = ["${valkey-data}:/data:U"];
+            volumes = ["${valkey-data}:/data"];
             networkAliases = ["valkey"];
             networks = ["searxng"];
             userns = "nomap";
             user = "valkey";
+            annotations = {"run.oci.keep_original_groups" = "1";};
           };
         };
         # https://github.com/searx/searx/discussions/1723#discussioncomment-832494
