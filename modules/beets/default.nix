0083a8ad58db885467bd650f4fccf6a80c2a0001
p-laranjinha <plcasimiro2000@gmail.com>
2025-12-30 21:30:32 +0000

    Replaced beets with picard.

--- modules/beets/default.nix
+++ modules/beets/default.nix
@@ -1,25 +1,29 @@
-# https://github.com/beetbox/beets
-# A terminal music tagger.
-{
-  pkgs,
-  funcs,
-  ...
-}: {
-  environment.systemPackages = with pkgs; [
-    beets
-    # python313Packages.beetcamp
-    essentia-extractor
-    ffmpeg
-
-    # A separate tool to calculate ReplayGain 2.0. Feels easier than using a
-    #  beets plugin.
-    rsgain
-
-    # A GUI music tagger just to have it.
-    picard
+{pkgs, ...}: {
+  # https://nixcademy.com/posts/mastering-nixpkgs-overlays-techniques-and-best-practice/
+  nixpkgs.overlays = [
+    (
+      final: prev: {
+        # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/pi/picard/package.nix#L86
+        picard = prev.picard.overrideAttrs (finalAttrs: prevAttrs: {
+          propagatedBuildInputs =
+            prevAttrs.propagatedBuildInputs
+            ++ (with prev.python312Packages; [
+              # Dependency for BPM Analyzer.
+              aubio
+            ]);
+        });
+      }
+    )
   ];
 
-  # Docs: https://beets.readthedocs.io/en/stable/
-  # Run `beet config -e` to edit this file.
-  hm.home.file.".config/beets/config.yaml".source = funcs.mkMutableConfigSymlink ./config.yaml;
+  environment.systemPackages = with pkgs; [
+    # A GUI music tagger just to have it.
+    picard
+
+    # Dependency for the ReplayGain 2.0 picard plugin.
+    rsgain
+
+    # Tool to mass download lrc lyrics for tracks.
+    lrcget
+  ];
 }
