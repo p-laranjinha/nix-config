c52f4de7a62cc844b04569703049b6b2406a6556
p-laranjinha <plcasimiro2000@gmail.com>
2026-02-26 18:41:40 +0000

    Niri progress + updated Neovim.

--- modules/niri/default.nix
+++ modules/niri/default.nix
@@ -9,17 +9,13 @@
   config = lib.mkIf config.opts.niri {
     environment.systemPackages = with pkgs; [
       xwayland-satellite
+      kdePackages.dolphin
+      nomacs # Image viewer
     ];
     programs = {
-      niri = {
-        enable = true;
-      };
-      dms-shell = {
-        enable = true;
-      };
-      dsearch = {
-        enable = true;
-      };
+      niri.enable = true;
+      dms-shell.enable = true;
+      dsearch.enable = true;
     };
     services.displayManager.dms-greeter = {
       enable = true;
@@ -33,5 +29,17 @@
       # Size doesn't seem to work with niri, set it in the config file.
       # size = 32;
     };
+
+    # TODO: keymaps
+    # TODO: background apps
+    # TODO: wallpapers (sync with greeter)
+    # TODO: dms json in this repo
+    # TODO: bar layout and plugins
+    # TODO: remove backup file
+    # TODO: dim right screen
+    # TODO: see if I can right click app widget for settings like fullscreen?
+    # TODO: fix fonts in apps like Mission Center
+    # TODO: setup calendar
+    # TODO: setup defaults like image viewer (File picker not working on https://wiki.nixos.org/wiki/Niri)
   };
 }
