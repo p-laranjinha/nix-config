47158dc4df3e2584aee9466f2baf98c75665a097
p-laranjinha <plcasimiro2000@gmail.com>
2026-03-03 21:04:17 +0000

    Improved greeter.
    Actually read the nixpkgs services and figured out how to disable the
    2nd monitor, and improve the old configuration.

--- modules/niri/greeter.nix
+++ modules/niri/greeter.nix
@@ -2,7 +2,6 @@
   lib,
   config,
   vars,
-  funcs,
   ...
 }:
 {
@@ -10,29 +9,46 @@
     services = {
       displayManager.dms-greeter = {
         enable = true;
-        compositor.name = "niri";
         configHome = vars.homeDirectory;
+        compositor = {
+          name = "niri";
+          customConfig = ''
+            // Taken from ./config/dms/outputs.kdl
+            output "DP-2" {
+                mode "2560x1440@165.002"
+                scale 1
+                position x=0 y=0
+                variable-refresh-rate on-demand=true
+                focus-at-startup
+            }
+            output "HDMI-A-1" {
+                off
+                mode "1920x1080@144.001"
+                scale 1
+                position x=2560 y=180
+                variable-refresh-rate on-demand=true
+            }
+            // Taken from https://danklinux.com/docs/dankgreeter/configuration#niri
+            hotkey-overlay {
+                skip-at-startup
+            }
+            environment {
+                DMS_RUN_GREETER "1"
+            }
+            gestures {
+              hot-corners {
+                off
+              }
+            }
+          '';
+        };
       };
     };
-
     # Sync greeter with DMS.
     users.users.${vars.username} = {
       extraGroups = [
         "greeter"
       ];
     };
-    systemd.tmpfiles.rules = [
-      "L+ /var/cache/dms-greeter/settings.json - - - - ${funcs.relativeToAbsoluteConfigPath ./dms-settings.json}"
-      "Z /var/cache/dms-greeter/settings.json 777 ${vars.username} greeter - -"
-      "Z ${funcs.relativeToAbsoluteConfigPath ./dms-settings.json} 777 ${vars.username} greeter - -"
-
-      "L+ /var/cache/dms-greeter/session.json - - - - ${vars.homeDirectory}/.local/state/DankMaterialShell/session.json"
-      "Z /var/cache/dms-greeter/session.json 777 ${vars.username} greeter - -"
-      "Z ${vars.homeDirectory}/.local/state/DankMaterialShell/session.json 777 ${vars.username} greeter - -"
-
-      "L+ /var/cache/dms-greeter/colors.json - - - - ${vars.homeDirectory}/.cache/DankMaterialShell/dms-colors.json"
-      "Z /var/cache/dms-greeter/colors.json 777 ${vars.username} greeter - -"
-      "Z ${vars.homeDirectory}/.cache/DankMaterialShell/dms-colors.json 777 ${vars.username} greeter - -"
-    ];
   };
 }
